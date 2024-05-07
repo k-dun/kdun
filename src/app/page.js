ebee77d6dc86dc2987ee972d5564ecac7152acda
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-05-07 23:58:57 +0100

    Created skeleton of the whole project with all needed components.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,29 @@
 "use client";
 
+import Navbar from './components/navbar/navbar';
+import Hero from './components/hero/hero';
+import About from './components/about/about';
+import Projects from './components/projects/projects';
+import Skills from './components/skills/skills';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+
 export default function Home() {
   return (
     <>
+      <header>
+        <Navbar />
+      </header>
       <main className="">
-      
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
