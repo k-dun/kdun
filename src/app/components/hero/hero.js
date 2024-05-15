9649543042cb8f5b28a3eae7498fbbe1bcc64d17
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-05-15 02:08:38 +0100

    Added Soft Skills section with all its content.

--- src/app/components/hero/hero.js
+++ src/app/components/hero/hero.js
@@ -22,7 +22,7 @@ export default function Hero() {
             </Link>
             <Link href="/Konrad_Dunski_CV.pdf" target="_blank">
               <button className="w-[42svw] font-semibold text-[#202020] bg-[#FCFAFF] text-base px-5 py-3 rounded-md border border-[#FF8080] mr-2 flex flex-row justify-center">
-                Resumé <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scroll-text" className="ml-2"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg>
+                <span className="pr-2">Resumé</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-scroll-text"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg>
               </button>
             </Link>
           </div>
--- src/app/components/skills/skills.js
+++ src/app/components/skills/skills.js
@@ -5,65 +5,154 @@ export default function Skills() {
       <section id="skills" className="w-[90svw] bg-[#FCFAFF] mx-auto flex flex-col justify-center mt-32 pb-4">
         <h3 className="w-[50svw] text-xl mb-16"><span className="text-[#FF8080]">02.1</span> Tech Skills</h3>
 
-	      <div class="grid w-full divide-y divide-[#FF8080] mx-auto">
-		      <div class="py-5">
-			      <details class="group">
-				      <summary class="flex justify-between items-center cursor-pointer list-none">
+	      <div className="grid w-full divide-y divide-[#FF8080] mx-auto">
+		      <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
 					      <span className="justify-start text-lg">FRONTEND</span>
-					      <span class="transition group-open:rotate-180">
-                  <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
                 </span>
 				      </summary>
-				      <p class="text-[#505050] text-base mt-3">
+				      <p className="text-[#505050] text-base mt-3">
 					      HTML, CSS, TailwindCSS, Javascript, React, Cypress, MaterialUI, ShadCN, DaisyUI
 				      </p>
 			      </details>
 		      </div>
 
-          <div class="py-5">
-			      <details class="group">
-				      <summary class="flex justify-between items-center cursor-pointer list-none">
-					      <span className="justify-start text-lg">BACKEND</span>
-					      <span class="transition group-open:rotate-180">
-                  <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg">BACKEND/DATABASE</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
                 </span>
 				      </summary>
-				      <p class="text-[#505050] text-base mt-3">
-					      Javascript, Node.js, Express.js, Jest, Ruby, RSpec
+				      <p className="text-[#505050] text-base mt-3">
+					      Javascript, Node.js, Express.js, Jest, Ruby, RSpec, MongoDB, PostgreSQL, SQL
 				      </p>
 			      </details>
 		      </div>
 
-          <div class="py-5">
-			      <details class="group">
-				      <summary class="flex justify-between items-center cursor-pointer list-none">
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
 					      <span className="justify-start text-lg">DESIGN/TOOLS</span>
-					      <span class="transition group-open:rotate-180">
-                  <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
                 </span>
 				      </summary>
-				      <p class="text-[#505050] text-base mt-3">
+				      <p className="text-[#505050] text-base mt-3">
 					      Canva, Figma, Github, VSCode
 				      </p>
 			      </details>
 		      </div>
 
-          <div class="py-5">
-			      <details class="group">
-				      <summary class="flex justify-between items-center cursor-pointer list-none">
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
 					      <span className="justify-start text-lg">METHODOLOGIES</span>
-					      <span class="transition group-open:rotate-180">
-                  <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
                 </span>
 				      </summary>
-				      <p class="text-[#505050] text-base mt-3">
-					      Agile, Test-Driven Development (TDD), Object-oriented Programming (OOP)
+				      <p className="text-[#505050] text-base mt-3">
+					      Agile, Test-Driven Development, Object-oriented Programming
 				      </p>
 			      </details>
 		      </div>
         </div>
 
         <h3 className="w-[50svw] text-xl my-16"><span className="text-[#FF8080]">02.2</span> Soft Skills</h3>
+
+        <div className="grid w-full divide-y divide-[#FF8080] mx-auto">
+		      <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg">COMMUNICATION</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+                </span>
+				      </summary>
+				      <p className="text-[#505050] text-base mt-3"> 
+                Throughout my career, I have learned the importance and benefits of clear 
+                communication.<br /><br /> It helped me develop a keen sensitivity to nuances in language and 
+                context, enhancing my capacity to convey ideas with clarity and precision.
+				      </p>
+			      </details>
+		      </div>
+
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg">LEADERSHIP</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+                </span>
+				      </summary>
+				      <p className="text-[#505050] text-base mt-3">
+                I have successfully led teams for 12+ years of my professional career. <br /><br /> 
+                It helped me hone my abilities in assessing people's strengths and motivating 
+                diverse groups towards common goals.
+				      </p>
+			      </details>
+		      </div>
+
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg">ADAPTABILITY</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+                </span>
+				      </summary>
+				      <p className="text-[#505050] text-base mt-3">
+                I have strong bias to action and I am always ready to adapt or pivot. <br /><br />
+                Tech's ever-changing landscape with its constant need for learning new frameworks 
+                and adjusting one's strategies is very in line with my strengths.
+				      </p>
+			      </details>
+		      </div>
+
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg">PROBLEM SOLVING</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+                </span>
+				      </summary>
+				      <p className="text-[#505050] text-base mt-3">
+					      One of the main reasons I decided to transition into tech is my love for problem solving. <br /><br />
+                Whether it was logistics and management in my previous line of work, coding challenges 
+                and dealing with persistent errors now, or cryptographic riddles I was obsessed with as a 
+                teenager. <br /><br />
+                Thanks to my innate curiosity and resilience, I found that the process itself always 
+                brings me enormous satisfaction.
+				      </p>
+			      </details>
+		      </div>
+
+          <div className="py-5">
+			      <details className="group">
+				      <summary className="flex justify-between items-center cursor-pointer list-none">
+					      <span className="justify-start text-lg text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-[#FF8080]">SECRET SUPERPOWER</span>
+					      <span className="transition group-open:rotate-180">
+                  <svg fill="none" height="24" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path></svg>
+                </span>
+				      </summary>
+				      <p className="text-[#505050] text-base mt-3">
+                Being an ambivert is possibly the root of all my greatest strengths.<br /><br />
+                My extrovert qualities and sense of humour make me a great team-player. I make 
+                everyone feel safe and heard in my presence. <br /><br />
+                The introvert side gave me great introspection skills, always allowing me to adapt 
+                and do what's necessary to solve whatever problem I'm presented with.<br /><br />
+                Both those combined help me find compromise where needed, as well as strength and 
+                assertiveness where necessary.
+				      </p>
+			      </details>
+		      </div>
+        </div>
       </section>
     </>
   );
