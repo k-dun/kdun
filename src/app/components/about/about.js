d014b3e45a366924a01ee104f2e680b836786008
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-05-29 03:07:09 +0100

    Adapted About, Contact and Footer to large screens.

--- src/app/components/about/about.js
+++ src/app/components/about/about.js
@@ -5,9 +5,9 @@ export default function About() {
 
   return (
     <>
-      <section id="about" className="w-[90svw] md:w-[70vw] bg-[#FCFAFF] mx-auto flex flex-col justify-center pt-[12svh] pb-4">
+      <section id="about" className="w-[90svw] md:w-[70vw] lg:w-[100vw] lg:ml-32 bg-[#FCFAFF] mx-auto flex flex-col justify-center pt-[12svh] pb-4">
         <h3 className="text-xl md:text-2xl font-semibold mb-16"><span className="text-[#FF8080]">03</span> ABOUT</h3>
-        <h4 className="text-lg md:text-xl">
+        <h4 className="text-lg md:text-xl lg:flex lg:flex-wrap lg:w-[70vw]">
           I always pursue my curiosity. Consuming a variety of tech related blogs and podcasts 
           combined with my long love of problem solving has eventually led me to finally leave 
           retail. <br /><br />
