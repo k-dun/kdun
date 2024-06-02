62e91add4f1478e294692c4455a632f43114e703
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-06-02 23:04:26 +0100

    Added more consistent text sizing depending on device size.

--- src/app/components/projects/ProjectCard.js
+++ src/app/components/projects/ProjectCard.js
@@ -6,21 +6,21 @@ const ProjectCard = ({ project }) => {
     <div className="w-[90svw] md:w-[70vw] lg:w-full flex flex-col rounded pt-6 lg:pt-10">
       <Image src={project.imageUrl} className="items-start rounded-t md:rounded-md md:w-[70vw] lg:w-[40vw]" width={800} height={450} alt={project.title} />
       <div className="lg:flex lg:flex-col lg:w-[40vw]">
-        <p className="font-semibold text-xl md:text-2xl px-3 md:px-0 pt-6">{project.title}</p>
-        <p className="text-lg md:text-xl px-3 md:px-0 py-5 lg:flex-wrap">{project.description}</p>
+        <p className="font-semibold text-xl px-3 md:px-0 pt-6">{project.title}</p>
+        <p className="text-lg px-3 md:px-0 py-5 flex-wrap">{project.description}</p>
         <p className="px-2 md:px-0 flex flex-wrap">
           {project.tech.map((tech, index) => (
-            <span key={index} className="bg-[#202020] text-[#FCFAFF] text-sm md:text-base lg:text-lg shadow-md rounded-2xl px-6 lg:px-8 py-1.5 lg:py-2 mx-1 lg:mx-0 lg:mr-2 my-1 lg:my-2 lg:flex-wrap">
+            <span key={index} className="bg-[#202020] text-[#FCFAFF] text-sm md:text-base shadow-md rounded-2xl px-6 lg:px-8 py-1.5 lg:py-2 mx-1 lg:mx-0 lg:mr-2 my-1 lg:my-2 lg:flex-wrap">
               {tech}
             </span>
           ))}
         </p>
         <div className="w-full flex flex-row py-8 pr-8 pl-3 md:pl-0 justify-around md:justify-start">
           <Link href={project.codeUrl} target="_blank">
-            <button className="w-[38svw] md:w-[26vw] lg:w-[12vw] font-semibold text-[#202020] bg-[#FCFAFF] shadow-md hover:shadow-xl text-base md:text-lg px-5 py-3 rounded-md border border-[#202020] justify-center mr-2 md:mr-4">Code</button>
+            <button className="w-[38svw] md:w-[26vw] lg:w-[12vw] font-semibold text-[#202020] bg-[#FCFAFF] shadow-md hover:shadow-xl text-base px-5 py-3 rounded-md border border-[#202020] justify-center mr-2 md:mr-4">Code</button>
           </Link>
           <Link href={project.liveUrl} target="_blank">
-            <button className="w-[38svw] md:w-[26vw] lg:w-[12vw] font-semibold text-[#202020] bg-[#F4B7AA] shadow-md hover:shadow-xl text-base md:text-lg px-5 py-3 rounded-md border border-[#202020] justify-center">Live</button>
+            <button className="w-[38svw] md:w-[26vw] lg:w-[12vw] font-semibold text-[#202020] bg-[#F4B7AA] shadow-md hover:shadow-xl text-base px-5 py-3 rounded-md border border-[#202020] justify-center">Live</button>
           </Link>
         </div>
       </div>
