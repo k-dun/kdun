0be06ea07bff1a4bab932babdfaa431e02f42711
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-06-05 23:12:52 +0100

    Fixed a link on the Hero.

--- src/app/components/hero/hero.js
+++ src/app/components/hero/hero.js
@@ -15,14 +15,14 @@ export default function Hero() {
             My name is Konrad and I am a frontend engineer based in London. I am passionate about UI/UX, web 
             design, startups and all things tech.</h1>
           <div className="w-[90svw] md:w-[70vw] lg:w-[40vw] flex flex-row pt-4 md:pt-6">
-            <Link href="/#about">
+            <Link href="/#projects">
               <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#FCFAFF] text-base px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl flex flex-row justify-center mr-3">
                 <span className="text-[#FF8080] mr-2">01</span>Projects
               </button>
             </Link>
             <Link href="/Konrad_Dunski_CV.pdf" target="_blank">
               <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#F4B7AA] text-base px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl mr-2 flex flex-row justify-center">
-                <span className="pr-2 bg-[#F4B7AA]">Resumé</span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-scroll-text bg-[#F4B7AA]"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-scroll-text bg-[#F4B7AA]"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg><span className="pl-2 bg-[#F4B7AA]">Resumé</span>
               </button>
             </Link>
           </div>
