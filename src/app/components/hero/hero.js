f35d2dd4a4e0539f322729eb6efb904c5198c9c3
Konrad Dunski <44006045+k-dun@users.noreply.github.com>
2024-06-10 03:18:35 +0100

    Random fixes.

--- src/app/components/hero/hero.js
+++ src/app/components/hero/hero.js
@@ -11,18 +11,18 @@ export default function Hero() {
         </div>
         <div className="w-[90svw] md:w-[70vw] lg:w-[50vw] pt-12 md:pt-16 lg:pt-18 flex flex-col items-center justify-center">
           <h2 className="w-[90svw] md:w-[70vw] lg:w-[40vw] text-2xl md:text-3xl lg:text-4xl text-center lg:text-left font-semibold">FRONTEND DEVELOPER</h2>
-          <h1 className="w-[90svw] md:w-[70vw] lg:w-[40vw] text-lg md:text-xl mt-8 lg:mt-8"><span className="text-[#FF8080]">00 </span>
+          <h1 className="w-[90svw] md:w-[70vw] lg:w-[40vw] text-lg md:text-xl mt-8 lg:mt-10"><span className="text-[#FF8080]">00 </span>
             My name is Konrad and I am a frontend engineer based in London. I am passionate about UI/UX, web 
             design, startups and all things tech.</h1>
           <div className="w-[90svw] md:w-[70vw] lg:w-[40vw] flex flex-row pt-4 md:pt-6">
             <Link href="/#projects">
-              <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#FCFAFF] text-base px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl flex flex-row justify-center mr-3">
+              <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#FCFAFF] text-base md:text-lg px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl flex flex-row justify-center mr-3">
                 <span className="text-[#FF8080] mr-2">01</span>Projects
               </button>
             </Link>
             <Link href="/Konrad_Dunski_CV.pdf" target="_blank">
-              <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#F4B7AA] text-base px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl mr-2 flex flex-row justify-center">
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-scroll-text bg-[#F4B7AA]"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg><span className="pl-2 bg-[#F4B7AA]">Resumé</span>
+              <button className="w-[42svw] md:w-[26vw] lg:w-[16vw] font-semibold text-[#202020] bg-[#F4B7AA] text-base md:text-lg px-5 py-3 rounded-md border border-[#202020] shadow-md hover:shadow-xl mr-2 flex flex-row justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-scroll-text bg-[#F4B7AA]"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg><span className="pl-2 bg-[#F4B7AA]">Resumé</span>
               </button>
             </Link>
           </div>
--- src/app/components/projects/projects.js
+++ src/app/components/projects/projects.js
@@ -39,7 +39,7 @@ export default function Projects() {
 
   return (
     <>
-      <section id="projects" className="w-[90svw] md:w-[70vw] lg:w-full lg:px-36 lg:mx-auto bg-[#FCFAFF] mx-auto pt-[14vh] flex flex-col justify-center items-center pb-4">
+      <section id="projects" className="w-[90vw] md:w-[70vw] lg:w-full lg:px-36 lg:mx-auto bg-[#FCFAFF] mx-auto pt-[14vh] flex flex-col justify-center items-center pb-4">
         <h3 className="text-xl self-start md:text-2xl mb-10 font-semibold"><span className="text-[#FF8080]">01</span> PROJECTS</h3>
         <div className="lg:grid lg:grid-cols-2 lg:gap-12 lg:self-start">
           {projects.map((project, index) => (
